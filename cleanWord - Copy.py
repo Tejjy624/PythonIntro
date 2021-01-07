106d40a36d3a73a7841313ae5d4a5fac59973a8a
Tejjy624 <54224255+Tejjy624@users.noreply.github.com>
2021-01-07 14:35:30 -0800

    Delete cleanWord - Copy.py

--- cleanWord - Copy.py
+++
@@ -1,9 +0,0 @@
-# module to remove extra characters from words that show up in typical English text.
-import string
-
-def clean(word):
-        # convert whole word to lower case
-        word = word.lower()
-        # trim off punctuation at the end and beginning
-        word = word.strip(string.punctuation)
-        return word
